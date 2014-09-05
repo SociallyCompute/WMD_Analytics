d563ec75599a1bdb9d09a1312a26a180f4228376
gps787 <periclesgiannaris@mwc-119031.dhcp.missouri.edu>
2014-09-05 16:45:53 -0500

    tnet_igraph and degree_w
    Plot and the number of contacts that a node is connected to

--- TnET-V1.R
+++ TnET-V1.R
@@ -9,6 +9,7 @@ jan.labels <- jan.labels[order(jan.labels)]
 jan[,"source"] <- factor(x=jan[,"source"], levels=jan.labels)
 jan[,"target"]   <- factor(x=jan[,"target"], levels=jan.labels)
 jan <- data.frame(i=as.integer(jan[,"source"]), j=as.integer(jan[,"target"]), w=jan[,"weight"])
+#the weight is also converted to integer while trouble shooting in SQL
 
 # Add up duplicated entries (multiple routes)
 jan <- jan[order(jan[,"i"], jan[,"j"]),]
@@ -16,7 +17,7 @@ index <- !duplicated(jan[,c("i","j")])
 jan <- data.frame(jan[index,c("i","j")], w=tapply(jan[,"w"], cumsum(index), sum))
 
 # Take out routes with no passengers (cargo)
-jan <- jan[jan[,"w"]>0,]
+jan <- jan[jan[,"w"]>0,] #>0 cause there should be no 0 in the weights
 
 # Take out routes from an airport to itself
 jan <- jan[jan[,"i"]!=jan[,"j"],]
@@ -26,6 +27,12 @@ library(tnet)
 jan <- na.omit(jan)
 jan <- as.tnet(jan, type="weighted one-mode tnet")
 
-jan2 <- as.tnet(jan, type="weighted one-mode tnet")
+jan2 <- as.tnet(jan, type="weighted one-mode tnet") #just by coding the type as it is R checks to make sure that the net is one way. . .
 
 btTest <- betweenness_w(jan)
+
+testc<-closeness_w(jan)
+
+testigraph<- tnet_igraph(jan, type="weighted one-mode tnet")
+
+degree_w(jan)
