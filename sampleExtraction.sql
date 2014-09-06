f7022eb09573b2b37a158541fe17e83863fa236a
Sean P. Goggins <s@goggins.com>
2014-09-06 08:15:33 -0500

    Fixed Extraction SQL
    Weights need to be integer values for TNET, so I moved the decimal four
    places to the right.

--- sampleExtraction.sql
+++ sampleExtraction.sql
@@ -1,5 +1,6 @@
-select poster as "source", creator as "target", round((log((1+(1/weight_in_minutes)))*10000), 4) as weight
+-- ensured the weight is an integer
+select poster as "source", creator as "target", round((log((1+(1/weight_in_minutes)))*1000000), 0) as weight
 from WMD_Three
 where month(date) = 1 and year(date) = 2010 and qid != -1 and creator is not null
 group by poster, creator
-order by qid, localID, date  
+order by qid, localID, date 
